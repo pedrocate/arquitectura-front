95ad348ff78f78b925e336553aaf4d995e973019
Pedro <pedro.catedra@collab-sunmedia.com>
2022-11-20 18:56:30 +0100

    chore: small changes

--- src/plugins/todoUseCases.ts
+++ src/plugins/todoUseCases.ts
@@ -2,6 +2,7 @@ import useTodo from "@/useCases/useTodo";
 import TodoLocalStorageRepository from "@/models/TodoLocalStorageRepository";
 import TodoRepository from "@/models/TodoRepository";
 
-const repository: TodoRepository = new TodoLocalStorageRepository();
-
-export default () => useTodo(repository);
+export default () => {
+  const repository: TodoRepository = new TodoLocalStorageRepository();
+  return useTodo(repository);
+};
