5c77922a38785638c62db77295add5125a77bb29
Pedro <pedro.catedra@collab-sunmedia.com>
2022-11-20 20:39:23 +0100

    chore: rename ITodoRepository

--- src/Todo/infrastructure/plugins/todoUseCases.ts
+++ src/Todo/infrastructure/plugins/todoUseCases.ts
@@ -1,10 +1,10 @@
 import useTodo from "@/Todo/application/useTodo";
-import TodoRepository from "@/Todo/domain/models/TodoRepository";
+import ITodoRepository from "@/Todo/domain/ITodoRepository";
 //import TodoLocalStorageRepository from "@/Todo/infrastructure/repository/TodoLocalStorageRepository";
 //import TodoVuexRepository from "../repository/TodoVuexRepository";
 import TodoPiniaRepository from "../repository/TodoPiniaRepository";
 
 export default () => {
-  const repository: TodoRepository = new TodoPiniaRepository();
+  const repository: ITodoRepository = new TodoPiniaRepository();
   return useTodo(repository);
 };
--- src/Todo/infrastructure/repository/TodoPiniaRepository.ts
+++ src/Todo/infrastructure/repository/TodoPiniaRepository.ts
@@ -1,8 +1,8 @@
-import Todo from "@/Todo/domain/models/Todo";
-import TodoRepository from "@/Todo/domain/models/TodoRepository";
+import Todo from "@/Todo/domain/Todo";
+import ITodoRepository from "@/Todo/domain/ITodoRepository";
 import { TodoStore, useTodoStore } from "@/Todo/infrastructure/store/pinia";
 
-export default class TodoPiniaRepository implements TodoRepository {
+export default class TodoPiniaRepository implements ITodoRepository {
   private store: TodoStore = useTodoStore();
 
   public getAll(): Todo[] {
